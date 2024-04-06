44bdc07e5f1372688d50ebe9f4b267f775d9b17b
Guptaashish2003 <ashish.10119011622@ipu.ac.in>
2024-04-06 13:12:27 +0530

    setting follower and following

--- src/app/api/user/follows/following/allFollowing/[id]/route.ts
+++ src/app/api/user/follows/following/allFollowing/[id]/route.ts
@@ -1,19 +1,16 @@
-
 import { dataBasePrisma } from "@/databasePrisma";
 import { NextResponse, NextRequest } from "next/server";
 
-import { currentRole } from "@/lib/authDet";
-import { currentUserId } from "@/lib/authDet";
 
-export async function GET(req: NextRequest,context: { params: { id: string } }) {
+export async function GET(req:NextRequest,context:{params:{id:string}}){
     try {
-        const writerId = context.params.id;
-        // find all user details who follows the writerid
-        const allFollowers = await dataBasePrisma.user.findMany({
+        const userId = context.params.id;
+        // find all writers  which userId follows the other writer or user
+        const allFollowing = await dataBasePrisma.user.findMany({
             where:{
                 following:{
                     some:{
-                        followingId:writerId
+                        followingId:userId
                     }
                 }
             },
@@ -24,11 +21,15 @@ export async function GET(req: NextRequest,context: { params: { id: string } }) 
                 username:true,
                 role:true
             }
+        
+
         });
-           
-        const length = allFollowers.length;
-        return NextResponse.json({success:true,followers:length,allFollowers},{status:200})
+        console.log(allFollowing)
+        const followingLength = allFollowing.length;
+        return NextResponse.json({success:true,following:followingLength,data:{allFollowing}},{status:200})
+        
     } catch (error) {
         return NextResponse.json({success:false,message:error},{status:400})
     }
+
 }
