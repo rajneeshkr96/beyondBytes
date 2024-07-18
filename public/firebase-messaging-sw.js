551136d9a76509d1953320e73887a843e64f6f52
rajnish <rajneeshkumar9674737@gmail.com>
2024-07-18 16:00:02 +0530

    upd:push notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,8 @@
-// public/firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+// Replace these with your own Firebase config keys...
 
 const firebaseConfig = {
   apiKey: "AIzaSyCsNjrXVg9NF1vY9GWg0VRYuxwJ9TO51Sw",
@@ -15,14 +17,44 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload);
+  console.log(
+    "[firebase-messaging-sw.js] Received background message",
+    payload
+  );
+
+  const link = payload.fcmOptions?.link || payload.data?.link;
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
+    icon: "./logo.png",
+    data: { url: link },
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+self.addEventListener("notificationclick", function (event) {
+  console.log("[firebase-messaging-sw.js] Notification click received.");
+  event.notification.close();
+
+  event.waitUntil(
+    clients
+      // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then(function (clientList) {
+        const url = event.notification.data.url;
+        if (!url) return;
+
+        for (const client of clientList) {
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          console.log("OPENWINDOW ON CLIENT");
+          return clients.openWindow(url);
+        }
+      })
+  );
+});
+
 
