f2d21c17f56f5379d63bef8532538b97fababcd7
rajnish <rajneeshkumar9674737@gmail.com>
2024-08-12 13:56:10 +0530

    actionTest3

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-nesting': {}, // Add nesting support
-    'tailwindcss': {},
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-}
+};
