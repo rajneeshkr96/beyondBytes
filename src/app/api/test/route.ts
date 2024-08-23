2de2e53ddb435e94e9c96e71f149b90caca155d3
Guptaashish2003 <117150498+Guptaashish2003@users.noreply.github.com>
2024-08-23 18:40:56 +0530

    Update route.ts

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,6 +2,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+    console.log('BASE_URL:', process.env.BASE_URL);
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
+    console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+    console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
+    console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
+    console.log('UPLOADTHING_APP_ID:', process.env.UPLOADTHING_APP_ID);
+    console.log('UPLOADTHING_SECRET:', process.env.UPLOADTHING_SECRET);
+
     try {
         return NextResponse.json({ success: true, message: "Token saved successfully" }, { status: 200 });
     } catch (error) {
