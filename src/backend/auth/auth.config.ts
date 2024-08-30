98bb4281b36ec4a4db7dee115a7393c58663e1e8
rajnish <rajneeshkumar9674737@gmail.com>
2024-08-30 12:11:30 +0530

    fixed:google auth

--- src/backend/auth/auth.config.ts
+++ src/backend/auth/auth.config.ts
@@ -1,8 +1,7 @@
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
-import { dataBasePrisma } from "../../../prisma/databasePrisma";
-import { UserRole } from "@prisma/client";
 
 const nextAuthConfig: NextAuthConfig = {
   providers: [
@@ -15,69 +14,16 @@ const nextAuthConfig: NextAuthConfig = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  trustHost: true,
+  // trustHost: true,
+  
 
+  // Explicitly set the NEXTAUTH_URL if necessary
   callbacks: {
     async redirect({ url, baseUrl }) {
       console.log(process.env.NEXTAUTH_URL,url, baseUrl,"......jlsdfjlksdjflkjfsdlk");
       return process.env.NEXTAUTH_URL || baseUrl;
     },
-    async session({ session, token }) {
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-      if (token.role && session.user) {
-        session.user.role = token.role as UserRole;
-      }
-      if (session.user) {
-        session.user.userId = token.userId as string;
-        session.user.name = token.name;
-        session.user.userName = token.userName as string;
-        session.user.email = token.email as string;
-        session.user.image = token.picture as string;
-        session.user.isOAuth = token.isOAuth as boolean;
-      }
-      return session;
-    },
-    async jwt({ token, user }) {
-      try {
-        await dataBasePrisma.$connect();
-        if (!token.sub) return token;
-
-        const existingUser = await dataBasePrisma.user.findUnique({
-          where: { email: token.email as string },
-        });
-        if (!existingUser) {
-          const userName = token?.email?.split('@')[0];
-          const newUser = await dataBasePrisma.user.create({
-            data: {
-              username: userName,
-              email: token.email,
-              name: token.name,
-              role: UserRole.USER,
-              image: token.picture,
-            },
-          });
-          token.role = newUser.role;
-          token.name = newUser.name;
-          token.email = newUser.email;
-          token.userId = newUser.id;
-          token.userName = newUser.username;
-          return token;
-        }
-
-        token.name = existingUser.name;
-        token.email = existingUser.email;
-        token.role = existingUser.role;
-        token.userId = existingUser.id;
-        token.image = existingUser.image;
-        token.userName = existingUser.username;
-        return token;
-      } finally {
-        await dataBasePrisma.$disconnect();
-      }
-    },
   },
 };
 
-export default nextAuthConfig;
+export default nextAuthConfig satisfies NextAuthConfig;
--- src/backend/auth/auth.ts
+++ src/backend/auth/auth.ts
@@ -1,12 +1,85 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { dataBasePrisma } from "../../../prisma/databasePrisma";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
-} = NextAuth(authConfig);
+} = NextAuth({
+  
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
 
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      if (session.user) {
+        session.user.userId = token.userId as string;
+        session.user.name = token.name;
+        session.user.userName = token.userName as string;
+        session.user.email = token.email as string;
+        session.user.image = token.picture as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      try {
+        dataBasePrisma.$connect();
+        if (!token.sub) return token;
+        const existingUser = await dataBasePrisma.user.findUnique({
+          where: { email: token.email as string},
+        });
+        var newUser;
+        
+        
+        
+        if (!existingUser) {
+          const userName = token?.email?.split('@')[0];
+          newUser = await dataBasePrisma.user.create({
+            data: {
+              username: userName,
+              email: token.email,
+              name: token.name,
+              role: UserRole.USER,
+              image: token.picture,
+            },
+          });
+          token.role = newUser.role;
+          token.name = newUser.name;
+          token.email = newUser.email;
+          token.userId = newUser.id;
+          token.userName = newUser.username;
+
+          return token;
+        }
+        token.name = existingUser.name;
+        token.email = existingUser.email;
+        token.role = existingUser.role;
+        token.userId = existingUser.id;
+        token.image = existingUser.image;
+        token.userName = existingUser.username;
+
+        return token;
+      } finally {
+        await dataBasePrisma.$disconnect();
+      }
+    },
+
+  },
+
+  session: { strategy: "jwt" },
+  
+
+  ...authConfig,
+});
 
 
